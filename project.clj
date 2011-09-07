4d4ce9e424105f650c232199561a5cb40a31e731
Robert Marianski <rob@marianski.com>
2011-09-06 20:21:59 -0400

    update dependencies and bump version

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
-(defproject rmarianski/ring-jetty-servlet-adapter "0.0.1"
-  :description "ring jetty adapter using a jetty servlet context"
+(defproject rmarianski/ring-jetty-servlet-adapter "0.0.2"
+  :description "ring jetty adapter that exposes servlet related information in the request map"
   :url "http://github.com/rmarianski/ring-jetty-servlet-adapter"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.mortbay.jetty/jetty "6.1.26"]
+  :dependencies [[org.mortbay.jetty/jetty "6.1.26"]
                  [org.mortbay.jetty/jetty-util "6.1.26"]
-                 [ring/ring-servlet "0.3.8"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]
-                     [clj-http "0.1.3"]
-                     [lein-clojars "0.6.0"]])
+                 [ring/ring-servlet "0.3.11"]]
+  :dev-dependencies [[clj-http "0.1.3"]])
